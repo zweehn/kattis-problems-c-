a122c0f763f76824a9f215cbead5af2e14794cd9
Sven Hosser <sven.hosser@web.de>
2024-01-29 21:26:46 +0100

    hmmmmmmmmmmmmmmmmmm

--- Virus/Program.cs
+++ Virus/Program.cs
@@ -5,34 +5,32 @@ using System.Linq;
 string line1 = Console.ReadLine();
 string line2 = Console.ReadLine();
 
-int start = 0;
-int end = 10 ^ 5;
+Dictionary<(string, string), int> cache = new();
 
-if (line1 == line2)
+int levenstein(string a, string b)
 {
-    Console.WriteLine("0");
-    return 0;
+    if (cache.ContainsKey((a, b))) return cache[(a, b)];
+    var result = levensteinInner(a, b);
+    cache[(a, b)] = result;
+
+    return result;
 }
 
-for (int i = 0; i < Math.Min(line1.Length, line2.Length); i++)
+int levensteinInner(string a, string b)
 {
-    if (line1[i] != line2[i])
-    {
-        start = i;
-        break;
-    }
+    if (a.Length == 0) return b.Length;
+    if (b.Length == 0) return a.Length;
+    if (a[0] == b[0]) return levenstein(a[1..], b[1..]);
+
+    Console.WriteLine(a + " " + b);
+
+    int min = int.MaxValue;
+    min = Math.Min(min, levenstein(a[1..], b));
+    //min = Math.Min(min, 1 + levenstein(a[1..], b[1..]));
+    min = Math.Min(min, 1 + levenstein(a, b[1..]));
+    return min;
 }
 
-
-for (int i = 1; i < Math.Min(line1.Length, line2.Length); i++)
-{
-    if (line1[line1.Length - i] != line2[line2.Length - i])
-    {
-        end = line2.Length - i;
-        break;
-    }
-}
-
-Console.WriteLine(end - start + 1);
+Console.WriteLine(levenstein(line1, line2));
 
 return 0;
