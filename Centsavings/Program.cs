2613960d80248388b9eb7f085c9db4daf6d0c5dc
Sven Hosser <sven.hosser@web.de>
2024-02-11 17:49:02 +0100

    testproiject

--- Centsavings/Program.cs
+++ Centsavings/Program.cs
@@ -11,44 +11,72 @@ var d = int.Parse(line1.Split(" ")[1]);
 
 int[] items = line2.Split(" ").Select(i => int.Parse(i)).ToArray();
 
-Dictionary<(int, int), int> memory = new Dictionary<(int, int), int>();
+Dictionary<(int, int), int> memo = new();
 
-int minValue(int to, int dividers)
+// base cases, n=0
+for (int i = 0; i <= d; i++)
 {
-    if (memory.ContainsKey((to, dividers)))
+    memo[(0, i)] = 0;
+}
+
+// base cases, d=0
+for (int i = 1; i <= n; i++)
+{
+    memo[(i, 0)] = memo[(i - 1, 0)] + items[i - 1];
+}
+
+// Recurrence
+for (int i = 1; i <= n; i++)
+{
+    for (int j = d; j > 0; j--)
     {
-        return memory[(to, dividers)];
-    }
-    else
-    {
-        int value = minValueInner(to, dividers);
-        memory[(to, dividers)] = value;
-        return value;
+        memo[(i, j)] = Math.Min(memo[(i - 1, j)] + items[i - 1], round(memo[(i - 1, j - 1)] + items[i - 1]));
     }
 }
 
-int minValueInner(int to, int dividers)
+// Extract answer
+int ans = int.MaxValue;
+for (int i = 0; i <= d; i++)
 {
-    if (to == 0)
-    {
-        return 0;
-    }
-    if (dividers == 0)
-    {
-        return minValue(to - 1, 0) + items[to];
-    }
-    else
-    {
-        int min = int.MaxValue;
-        for (int i = 0; i < to; i++)
-        {
-            min = Math.Min(min, round(minValue(i, dividers - 1)) + round(items[i..to].Sum()));
-            //Console.WriteLine(dividers + " Min is " + min + "Value of " + i + " is: " + round(minValue(i, dividers - 1)) + " and " + round(items[(i)..to].Sum()));
-        }
-        return min;
-    }
-
+    ans = Math.Min(ans, round(memo[(n, i)]));
 }
+
+Console.WriteLine(ans);
+
+// int minValue(int to, int dividers)
+// {
+//     if (memo.ContainsKey((to, dividers)))
+//     {
+//         return memo[(to, dividers)];
+//     }
+//     else
+//     {
+//         int value = minValueInner(to, dividers);
+//         memo[(to, dividers)] = value;
+//         return value;
+//     }
+// }
+
+// int minValueInner(int to, int dividers)
+// {
+//     if (to == 0)
+//     {
+//         return 0;
+//     }
+//     if (dividers == 0)
+//     {
+//         return items[0..to].Sum();
+//     }
+
+//     int min = int.MaxValue;
+//     for (int i = 0; i < to; i++)
+//     {
+//         min = Math.Min(min, round(minValue(i, dividers - 1)) + round(items[i..to].Sum()));
+//         //Console.WriteLine(dividers + " Min is " + min + "Value of " + i + " is: " + round(minValue(i, dividers - 1)) + " and " + round(items[(i)..to].Sum()));
+//     }
+//     return min;
+// }
+// Console.WriteLine(minValue(items.Length, d));
 
 int round(int value)
 {
@@ -63,6 +91,5 @@ int round(int value)
     }
 }
 
-Console.WriteLine(minValue(items.Length, d));
 
 return 0;
