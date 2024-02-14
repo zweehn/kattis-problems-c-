76a0aac25848fc662f49f71742e116f6d31bbfb3
Sven Hosser <sven.hosser@web.de>
2024-02-14 20:16:42 +0100

    orders and acm

---
+++ ACM/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+string line = null;
+var tries = new Dictionary<string, int>();
+var totalTime = 0;
+var totalProblems = 0;
+while ((line = Console.ReadLine()) != "-1")
+{
+    var submission = line.Split(" ");
+    var duration = int.Parse(submission[0]);
+    var problem = submission[1];
+    var right = submission[2] == "right";
+
+    if (!right)
+    {
+        tries[problem] = (tries.ContainsKey(problem) ? tries[problem] : 0) + 1;
+    }
+    else
+    {
+        var problemTime = (tries.ContainsKey(problem) ? tries[problem] : 0) * 20 + duration;
+        totalTime += problemTime;
+        totalProblems += 1;
+    }
+
+}
+Console.WriteLine($"{totalProblems} {totalTime}");
---
+++ Orders/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+_ = Console.ReadLine();
+var items = Console.ReadLine().Split(" ").Select(i => int.Parse(i));
+_ = Console.ReadLine();
+var orders = Console.ReadLine().Split(" ").Select(i => int.Parse(i));
+
+HashSet<int> ambiguous = new HashSet<int>();
+var values = new Dictionary<int, int[]>();
+var maxOrder = orders.Max();
+
+foreach (var (item, index) in items.Select((item, index) => (item, index)))
+{
+    var prev = values.ToDictionary(k => k.Key, v => v.Value);
+
+    {
+        int value = 0;
+        while (value <= maxOrder)
+        {
+            value += item;
+            if (values.ContainsKey(value))
+            {
+                ambiguous.Add(value);
+            }
+            else
+            {
+                values[value] = (values.ContainsKey(value - item) ? values[value - item] : new int[0]).Concat(new int[] { index + 1 }).ToArray();
+            }
+        }
+    }
+
+    foreach (var (v, i) in prev)
+    {
+        int value = v;
+        while (value <= maxOrder)
+        {
+            value += item;
+            if (values.ContainsKey(value))
+            {
+                ambiguous.Add(value);
+            }
+            else
+            {
+                values[value] = values[value - item].Concat(new int[] { index + 1 }).ToArray();
+            }
+        }
+    }
+}
+
+foreach (var order in orders)
+{
+    if (ambiguous.Contains(order))
+    {
+        Console.WriteLine("Ambiguous");
+    }
+    else if (values.ContainsKey(order))
+    {
+        Console.WriteLine(string.Join(' ', values[order]));
+    }
+    else
+    {
+        Console.WriteLine("Impossible");
+    }
+}
+
