a08220b73dedb80839c35480eb6076debadfcd9b
Sven Hosser <sven.hosser@web.de>
2024-02-16 21:22:10 +0100

    mewofactor

---
+++ meowfactor/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+var num = long.Parse(Console.ReadLine());
+
+long res = 1;
+for (long i = 2; i < 128; i++)
+    if ((num % System.Numerics.BigInteger.Pow(i, 9)) == 0)
+        res = i;
+
+Console.WriteLine(res);
