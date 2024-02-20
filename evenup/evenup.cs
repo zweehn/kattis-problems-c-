e7403621ee4edf8cab0512506bc5ac93f44a04c8
Sven Hosser <sven.hosser@web.de>
2024-02-20 22:21:21 +0100

    Kutevi and evenup

---
+++ evenup/evenup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+int n = int.Parse(Console.ReadLine());
+
+var stack = Console.ReadLine().Split(" ").Select(s => int.Parse(s)).ToList();
+
+
+for (int i = 1; i < n; i++)
+{
+    if ((stack[i - 1] + stack[i]) % 2 == 0)
+    {
+        stack.RemoveRange(i - 1, 2);
+        n -= 2;
+        i = Math.Max(i - 2, 1);
+        if (i > 1)
+        {
+            i -= 2;
+        }
+        else
+        {
+            i -= 1;
+        }
+    }
+}
+
+Console.WriteLine(stack.Count);
---
+++ kutevi/kutevi.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+var line1 = Console.ReadLine().Split(" ");
+var n = int.Parse(line1[0]);
+var k = int.Parse(line1[1]);
+
+var options = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+var challenges = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+int[] state = new int[n];
+
+var memo = new HashSet<(int, int)>();
+for (int i = 0; i < k; i++)
+{
+    Console.WriteLine(backtrack(challenges[i], 0) ? "YES" : "NO");
+}
+
+
+bool backtrack(int challenge, int step)
+{
+    if (memo.Contains((challenge, step)))
+    {
+        return false;
+    }
+    if (backtrack_inner(challenge, step))
+    {
+        return true;
+    }
+    else
+    {
+        memo.Add((challenge, step));
+        return false;
+    }
+}
+
+bool backtrack_inner(int challenge, int step)
+{
+    if (challenge % 360 == 0)
+    {
+        return true;
+    }
+    if (step == n) return false;
+    for (int i = 0; i <= 5; i++)
+    {
+        state[step] = i;
+        if (backtrack(challenge - (i * options[step]), step + 1))
+        {
+            return true;
+        }
+
+        state[step] = -i;
+        if (backtrack(challenge - (-i * options[step]), step + 1))
+        {
+            return true;
+        }
+    }
+    return false;
+}
