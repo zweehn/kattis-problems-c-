b1b9243c4ad9d247d836e7b7e2f50b802fbe37c1
Sven Hosser <sven.hosser@web.de>
2024-02-22 17:46:57 +0100

    esej

---
+++ esej/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+string line = Console.ReadLine();
+var a = int.Parse(line.Split(" ")[0]);
+var b = int.Parse(line.Split(" ")[1]);
+
+int minWords = Math.Max(a, b / 2 + 1);
+
+char[] word = "aaaaaa".ToCharArray();
+
+for (int i = 0; i < minWords; i++)
+{
+    GenerateWord();
+}
+
+void GenerateWord()
+{
+    word[0] = (char)(word[0] + 1);
+    int step = 0;
+
+    while (word[step] > 'z')
+    {
+        word[step] = 'a';
+        step = step + 1;
+        word[step] = (char)(word[step] + 1);
+    }
+
+    foreach (char c in word) Console.Write(c);
+    Console.Write(" ");
+}
